c4bc44e8ea85d7d758711f527ac542457bc5eac0
Ishwar Rimal <ishwarrimal@192.168.1.6>
2025-08-04 22:01:24 +0530

    update the env variable usage

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 provider "aws" {
-    region = "eu-north-1"   
+    region = "eu-north-1"
+    access_key = var.aws_access_key_id
+    secret_key = var.aws_secret_access_key
+    
 }
 
 resource "aws_s3_bucket" "my_bucket" {
